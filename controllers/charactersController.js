845d22a6d8230c1fa749b927090c527ef41e58ba
Rifki Fauzi <kubido@gmail.com>
2021-06-26 21:23:18 +0700

    add redis cache

--- controllers/charactersController.js
+++ controllers/charactersController.js
@@ -1,22 +1,42 @@
 const { marvelAPI } = require('../lib/http')
+const redis = require('../lib/redis')
 
 const index = async (req, res, next) => {
   try {
-    const resp = await marvelAPI.get('/characters')
-    const characters = resp.data.data.results
-    const characterIds = characters.map(char => char.id)
-    res.json({ ids: characterIds })
+    const cachedKey = 'characters'
+    const cachedResponse = await redis.get(cachedKey)
+    if (cachedResponse) {
+      res.status(200).json(JSON.parse(cachedResponse))
+    } else {
+      const resp = await marvelAPI.get('/characters', {
+        params: { limit: 100 }
+      })
+      const characters = resp.data.data.results
+      const characterIds = characters.map(char => char.id)
+      const response = { ids: characterIds }
+      redis.set(cachedKey, JSON.stringify(response), 'EX', 3600)
+      res.status(200).json(response)
+    }
+
   } catch (error) {
     next(error)
   }
 }
 
-const show = async (req, res) => {
+const show = async (req, res, next) => {
   try {
-    const resp = await marvelAPI.get(`/characters/${req.params.characterId}`)
-    const character = resp.data.data.results[0]
-    const { id, name, description } = character
-    res.json({ Id: id, Name: name, Description: description })
+    const cachedKey = `characters:${req.params.characterId}`
+    const cachedResponse = await redis.get(cachedKey)
+    if (cachedResponse) {
+      res.status(200).json(JSON.parse(cachedResponse))
+    } else {
+      const resp = await marvelAPI.get(`/characters/${req.params.characterId}`)
+      const character = resp.data.data.results[0]
+      const { id, name, description } = character
+      const response = { Id: id, Name: name, Description: description }
+      redis.set(cachedKey, JSON.stringify(response), 'EX', 3600)
+      res.status(200).json(response)
+    }
   } catch (error) {
     next(error)
   }
