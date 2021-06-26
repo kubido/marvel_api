15189115bd68fd61c7a20a422288e27b7370983d
Rifki Fauzi <kubido@gmail.com>
2021-06-26 20:51:31 +0700

    add basic error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -7,7 +7,8 @@ const errorHandler = (err, req, res, next) => {
 
     default:
       res.status(500).json({
-        message: "Something went wrong"
+        message: "Something went wrong",
+        description: err.message
       })
   }
 }
