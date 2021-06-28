8f5bc20325679a51fc2f29b1619e1fbf619bc9e7
Rifki Fauzi <kubido@gmail.com>
2021-06-28 11:07:49 +0700

    remove unused vars

--- routes/character.js
+++ routes/character.js
@@ -1,9 +1,6 @@
-const axios = require('axios')
 const router = require('express').Router()
 const characterController = require('../controllers/charactersController')
 const cacheValidation = require('../middlewares/cache')
-
-const { marvelAPI } = require('../lib/http')
 
 router.get('/', cacheValidation, characterController.index)
 router.get('/:characterId', cacheValidation, characterController.show)
